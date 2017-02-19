beee7f8e9b25ef3d798405e8c616f56bf41dffaf
Junyuan Xue <junyuanxue.is@gmail.com>
2017-02-19 18:38:44 +0000

    mapping shapes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,71 @@
-module.exports = {
-  BOARD_WIDTH: 10,
-  BOARD_HEIGHT: 20
+const initialGrid = []
+const BOARD_WIDTH = 10
+const BOARD_HEIGHT = 20
+for (let i = 0; i < BOARD_WIDTH; i++) {
+  initialGrid.push(['grey'])
+}
+for (let i = 0; i < BOARD_WIDTH; i++) {
+  for (let j = 0; j < BOARD_HEIGHT; j++) {
+    initialGrid[i].push('grey')
+  }
+}
+
+export default {
+  initialGrid,
+
+  shapesMapping: [
+    'i', 'o', 't', 'j', 'l', 'z', 's'
+  ],
+
+  tetrominoes: {
+    i: {
+      shape: [
+				[1, 1, 1, 1],
+				[0, 0, 0, 0],
+				[0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      color: 'red'
+    },
+
+    o: {
+      shape: [
+        [1, 1, 0, 0],
+				[1, 1, 0, 0],
+				[0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      color: 'blue'
+    },
+
+    t: {
+      shape: [
+        [0, 1, 0, 0],
+				[1, 1, 1, 0],
+				[0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      color: 'green'
+    },
+
+    j: {
+      shape: [
+        [0, 0, 1, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      color: 'orange'
+    },
+
+    l: {
+      shape: [
+        [1, 0, 0, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      color: 'yellow'
+    }
+  }
 }
