966a31f94057409f7ec0f1ccdb14197d5106ab3c
Junyuan Xue <junyuanxue.is@gmail.com>
2017-03-06 23:20:23 +0000

    set current tetromino

--- src/reducers/currentTetrominoReducer.js
+++ src/reducers/currentTetrominoReducer.js
@@ -1,10 +1,16 @@
 import { handleActions } from 'redux-actions'
 import { TetrominoState } from '../constants/state'
+import Constants from '../constants/constants'
+const { initialGrid, tetrominoes, blockUnit } = Constants
 
 const currentTetrominoReducer = handleActions({
-  SET_CURRENT_TETROMINO: (state, { payload }) => {
-    console.log(payload)
-  }
+  SET_CURRENT_TETROMINO: (state, { payload }) => ({
+    offsetX: 0,
+    offsetY: 0,
+    shape: tetrominoes[payload.randomShape].shape,
+    name: payload.randomShape,
+    color: tetrominoes[payload.randomShape].color
+  })
 }, TetrominoState) // currentTetrominoState
 
 export default currentTetrominoReducer
