57ebd14e76c18c6c42f8de7f216468ac488200f4
Junyuan Xue <junyuanxue.is@gmail.com>
2017-03-16 23:15:30 +0000

    Added MOVE_DOWN in currentTetrominoReducer :game:

--- src/reducers/currentTetrominoReducer.js
+++ src/reducers/currentTetrominoReducer.js
@@ -1,7 +1,7 @@
 import { handleActions } from 'redux-actions'
 import { TetrominoState } from '../constants/states'
 import constants from '../constants/constants'
-const { initialGrid, tetrominoes } = constants
+const { initialGrid, tetrominoes, BLOCK_SIZE } = constants
 
 const currentTetrominoReducer = handleActions({
   SET_CURRENT_TETROMINO: (state, { payload }) => ({
@@ -10,7 +10,10 @@ const currentTetrominoReducer = handleActions({
     shape: tetrominoes[payload.randomShape].shape,
     name: payload.randomShape,
     color: tetrominoes[payload.randomShape].color
-  })
+  }),
+  MOVE_DOWN: (state) => (
+    Object.assign({}, state, { offsetX: state.offsetX - BLOCK_SIZE })
+  )
 }, TetrominoState) // currentTetrominoState
 
 export default currentTetrominoReducer
