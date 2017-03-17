f8bafcc8d8671a38e372f8fe995fbd382dc5fce2
Junyuan Xue <junyuanxue.is@gmail.com>
2017-03-17 19:30:23 +0000

    checking collision

--- src/lib/detectBorders.js
+++ src/lib/detectBorders.js
@@ -1,20 +1,45 @@
-import constants from '../constants/constants'
+import { BLOCK_SIZE, BOARD_WIDTH, BOARD_HEIGHT } from '../constants/constants'
 
-const hasCollision = (direction) => {
+const hasCollision = (direction, tetromino) => {
+  let isColliding = false
+
+  const numOfBlocksX = BOARD_WIDTH / BLOCK_SIZE
+  const numOfBlocksY = BOARD_HEIGHT / BLOCK_SIZE
+  const currentX = tetromino.offsetX / BLOCK_SIZE
+  const currentY = tetromino.offsetY / BLOCK_SIZE
+  let changeX = 0
+  let changeY = 0
+
   switch (direction) {
     case 'left':
-
+      changeX = -1
       break
 
     case 'right':
+      changeX = 1
       break
 
-    case 'left':
+    case 'down':
+      changeY = 1
       break
 
     default:
       break
   }
+
+  const { shape } = tetromino
+  shape.forEach((row, i) => {
+    row.forEach((block, j) => {
+      let finalX = currentX + changeX + j
+      let finalY = currentY + changeY + i
+
+      if (finalX < 0 || finalX >= numOfBlocksX || finalY >= numOfBlocksY) {
+        isColliding = true
+      }
+    })
+  })
+
+  return isColliding
 }
 
 export default hasCollision
