7e7500d2e53eccdfe3596f2a8903779d0fb300c8
Junyuan Xue <junyuanxue.is@gmail.com>
2017-03-19 17:57:04 +0000

    updaing grid

--- src/lib/getUpdatedGrid.js
+++ src/lib/getUpdatedGrid.js
@@ -1,5 +1,30 @@
+import constants from '../constants/constants'
+const { BLOCK_SIZE } = constants
+
 const getUpdatedGrid = (grid, tetromino) => {
   const { color } = tetromino
+  const blockCoordinates = getBlockCoordinates(tetromino)
+  return ['1']
+}
+
+function getBlockCoordinates (tetromino) {
+  const blockCoordinates = []
+  const currentX = tetromino.offsetX / BLOCK_SIZE
+  const currentY = tetromino.offsetY / BLOCK_SIZE
+
+  const { shape } = tetromino
+  shape.forEach((row, y) => {
+    row.forEach((block, x) => {
+      if (block) {
+        blockCoordinates.push({
+          x: x + currentX,
+          y: y + currentY
+        })
+      }
+    })
+  })
+
+  return blockCoordinates
 }
 
 export default getUpdatedGrid
