f77c0916ad277a69b51d03fb78640725c9ffaf0c
Junyuan Xue <junyuanxue.is@gmail.com>
2017-03-19 16:52:48 +0000

    Set up TetrominoStack component

---
+++ src/components/Tetromino/TetrominoStack.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class TetrominoStack extends React.Component {
+  render () {
+    console.log(this.props)
+
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default TetrominoStack
