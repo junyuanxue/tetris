8c0de08534d5b598c36f7b31960e32ca41322c8f
Junyuan Xue <junyuanxue.is@gmail.com>
2017-03-19 17:16:47 +0000

    some spacing

--- src/reducers/tetrominoStackReducer.js
+++ src/reducers/tetrominoStackReducer.js
@@ -4,14 +4,13 @@ import constants from '../constants/constants'
 const { initialGrid } = constants
 
 const tetrominoStackReducer = handleActions({
-  START_TETROMINO_STACK: (state) => (
-    // state.set('tetrominoStack', initialGrid)
-    { grid: initialGrid }
-  ),
-  UPDATE_TETROMINO_STACK: (state, { payload }) => (
-    // state.set('tetrominoStack', payload)
-    { grid: payload }
-  )
+  START_TETROMINO_STACK: (state) => ({
+    grid: initialGrid
+  }),
+
+  UPDATE_TETROMINO_STACK: (state, { payload }) => ({
+    grid: payload
+  })
 }, TetrominoStackState)
 
 export default tetrominoStackReducer
