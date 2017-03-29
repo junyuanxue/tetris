ec233e5b740f5a2d3d84e0dc41d70c98a4e3feb1
Junyuan Xue <junyuanxue.is@gmail.com>
2017-03-29 22:21:12 +0100

    rotateShape function :head_bandage:

--- src/lib/rotateShape.js
+++ src/lib/rotateShape.js
@@ -1,12 +1,21 @@
 const rotateShape = (shape) => {
   const matrix = shape.map(value => [...value])
   const rotatedShape = [[], [], [], []]
+  let newStartIndex = 10
 
   matrix.forEach((row, y) => {
     row.forEach((block, x) => {
       rotatedShape[y][x] = matrix[4 - x - 1][y]
-      
+      if (rotatedShape[y][x]) {
+        newStartIndex = Math.min(newStartIndex, x)
+      }
     })
+  })
+
+  const zeros = new Array(newStartIndex).fill(0)
+
+  matrix.forEach((row, y) => {
+    rotateShape[y] = rotateShape[y].slice(newStartIndex).concat(zeros)
   })
 
   return rotatedShape
